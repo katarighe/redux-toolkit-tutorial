c5d5a555dce2147e26d87a06c5cea943556e57bb
katarighe <m.ighe@hotmail.com>
2023-08-21 16:05:40 +0300

    Update the cart container

--- starter/src/components/navbar/CartContainer.js
+++ starter/src/components/navbar/CartContainer.js
@@ -1,7 +1,41 @@
 import CartItem from "./CartItem";
+import { useSelector } from 'react-redux';
 
 const CartContainer = () => {
-    return <div>Cart Container</div>
+    const { cartitems, total, amount } = useSelector((store) => store.cart);
+
+    if(amount < 1) {
+        return ( 
+        <selection className='cart'>
+            <header>
+                <h2>Your Bag</h2>
+                <h4 className='empty-cart'>is currently empty</h4>
+            </header>
+        </selection>
+        );
+    }
+
+    return ( 
+    <section className='cart'>
+        <header>
+        <h2>your bag</h2>
+        </header>
+        <div>
+            {cartItems.map((item) => {
+             return <CartItem key={item.id} {...item} />
+            })}
+        </div>
+        <footer>
+        <div class='cart-total'>
+            <hr />
+            <h4>
+                total <span>${total}</span>
+            </h4>
+            </div>
+            <button className='clear-btn'>clear cart</button>
+        </footer>
+        </section>
+    );
 }; 
 
 export default CartContainer;
