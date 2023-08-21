8db47da9061654a4e95e085dda1c1570241d512a
katarighe <m.ighe@hotmail.com>
2023-08-21 17:16:08 +0300

    Update the Modal

--- starter/src/components/navbar/Modal.js
+++ starter/src/components/navbar/Modal.js
@@ -1,16 +1,32 @@
-import { useDispatch } from 'react-redux';;
-import { clearCart } from '../features/cart/cartSlice';
-import { closeModal } from '../features/modal/modalSlice';
+import { useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+import { closeModal } from "../features/modal/modalSlice";
+
 const Modal = () => {
+  const dispatch = useDispatch();
   return (
     <aside className="modal-container">
       <div className="modal">
         <h4>remove all items from your shoping cart</h4>
         <div className="button-container">
-          <button type="button" className="btn confirm-btn">
+          <button
+            type="button"
+            className="btn confirm-btn"
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
             confirm
           </button>
-          <button type="button" className="btn cancel-btn">
+          <button
+            type="button"
+            className="btn confirm-btn"
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
             cancel
           </button>
         </div>
