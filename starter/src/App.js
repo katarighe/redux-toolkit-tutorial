cdf9563644711498bad779ecc7d4ffe3172ead7e
katarighe <m.ighe@hotmail.com>
2023-08-21 17:39:24 +0300

    Update the changes relating to createAsync function

--- starter/src/App.js
+++ starter/src/App.js
@@ -2,18 +2,29 @@ import { useDispatch, useSelector } from "react-redux";
 import Navbar from "./components/navbar";
 import CartContainer from "./components/navbar/CartContainer";
 import { useEffect } from "react";
-import calculateTotals from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import Modal from './components/Modal';
 
 const { isOpen } = useSelector((state) => state.modal);
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [])
+
+  if(isLoading){
+    return <div className='loading'>
+      <h1>Loading...</h1>
+    </div>
+  }
 
   return (
     <main>
