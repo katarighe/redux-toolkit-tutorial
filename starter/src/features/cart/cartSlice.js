a95fa23265c5c0e0df940d2dea78d7beccac1195
katarighe <m.ighe@hotmail.com>
2023-08-21 16:43:18 +0300

    Update Toggle Amount

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -19,10 +19,18 @@ const cartSlice = createSlice({
       const itemID = action.payload;
       state.cartItems = state.cartItems.filter(() => item.id !== itemID);
     },
+    increase: (state, { payload }) => {
+        const cartItem = state.cartItems.find((item)=>item.id === payload.id)
+        cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+        const cartItem = state.cartItems.find((item)=>item.id === payload.id)
+        cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
 // console.log(cartSlice);
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
