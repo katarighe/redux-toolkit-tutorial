2cadf32d4ee347ea671e258a0c1041bebe6fe386
katarighe <m.ighe@hotmail.com>
2023-08-21 16:19:47 +0300

    Update the ClearCart feature

--- starter/src/components/navbar/CartContainer.js
+++ starter/src/components/navbar/CartContainer.js
@@ -1,41 +1,45 @@
 import CartItem from "./CartItem";
-import { useSelector } from 'react-redux';
+import { clearCart } from "../../features/cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartContainer = () => {
-    const { cartitems, total, amount } = useSelector((store) => store.cart);
+  const dispatch = useDispatch;
+  const { cartitems, total, amount } = useSelector((store) => store.cart);
 
-    if(amount < 1) {
-        return ( 
-        <selection className='cart'>
-            <header>
-                <h2>Your Bag</h2>
-                <h4 className='empty-cart'>is currently empty</h4>
-            </header>
-        </selection>
-        );
-    }
-
-    return ( 
-    <section className='cart'>
+  if (amount < 1) {
+    return (
+      <selection className="cart">
         <header>
-        <h2>your bag</h2>
+          <h2>Your Bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
         </header>
-        <div>
-            {cartitems.map((item) => {
-             return <CartItem key={item.id} {...item} />
-            })}
-        </div>
-        <footer>
-        <div class='cart-total'>
-            <hr />
-            <h4>
-                total <span>${total}</span>
-            </h4>
-            </div>
-            <button className='btn clear-btn'>clear cart</button>
-        </footer>
-        </section>
+      </selection>
     );
-}; 
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartitems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <div class="cart-total">
+          <hr />
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
+};
 
 export default CartContainer;
